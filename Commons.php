67934e873217269058260cc4042a0918f4abb2a6
jmsalash <jmsalash@gmail.com>
2016-03-23 15:25:50 +0000

    Update Commons.php
    lic.php is not needed anymore. Since it's not included in the code Commons.php was failing to include the file. Commented the 'require' command

--- Commons.php
+++ Commons.php
@@ -27,7 +27,7 @@ require_once('../../../config/config.inc.php');
 
 require_once('PSWebServiceLibrary.php');
 require_once('CRUDMapper.php');
-require_once('lic.php');
+//require_once('lic.php');
 /**
  * Class CommonReturn
  *
